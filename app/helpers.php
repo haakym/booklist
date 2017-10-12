6fc27d0f7a0833796cc3f4423c8935ce786ce863
Dominic <haakym@gmail.com>
2017-10-12 13:52:35 +0100

    include search params on sort

--- app/helpers.php
+++ app/helpers.php
@@ -3,5 +3,7 @@
 function sort_by_attr_link($link, $attr) {
 	$direction = request('direction') === 'asc' ? 'desc' : 'asc';
 
-	return route($link, ['page' => request('page'), 'sortBy' => $attr, 'direction' => $direction]);
+	$params = collect(request()->only('page', 'title', 'author'))->filter();
+
+	return route($link, $params->merge(['sortBy' => $attr, 'direction' => $direction])->all());
 }
