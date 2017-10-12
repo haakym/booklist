94ac2c22bdcb9e8b25fb1258b7b6c70c9fe11ef9
Dominic Jones <haakym@gmail.com>
2017-10-12 20:35:22 +0100

    tidy up test

--- tests/Unit/Helpers/SortByAttributeLinkTest.php
+++ tests/Unit/Helpers/SortByAttributeLinkTest.php
@@ -14,10 +14,8 @@ class SortByAttributeLinkTest extends TestCase
         $link = 'books.index';
         $attr = 'author';
         
-        // apply function on link, pass in attr
         $linkWithAttrSort = sort_by_attr_link($link, $attr);
         
-        // assert it returns the correct value
         $this->assertEquals('http://booklist.dev/books?sortBy=author&direction=asc', $linkWithAttrSort);
     }
 }
